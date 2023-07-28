ffe1f6df98b7fe65cc88ea24f0628fdab03354e8
ZzKotorizZ <lethanhvinh01645@gmail.com>
2023-07-28 23:00:14 +0700

    test luu code java

---
+++ App.java
@@ -0,0 +1,19 @@
+public class App {
+
+    
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+    }
+
+    
+
+    
+
+    
+    
+    
+
+
+}
